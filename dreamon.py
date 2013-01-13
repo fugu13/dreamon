f569a8038fafc08282fbdd9988361d47eb360b2e
Russell Duhon <fugu13@gmail.com>
2013-01-12 21:10:45 -0800

    getstudents

--- dreamon.py
+++ dreamon.py
@@ -29,7 +29,7 @@ def callback():
     client.request_token(code=request.args['code'])
     access_token = client.access_token
 
-    response = requests.get('https://api.sandbox.slcedu.org/api/rest/v1/teachers/537fab8373d843f3fedb7beab7c5988d628e2d17_id/teacherSectionAssociations/sections',
+    response = requests.get('https://api.sandbox.slcedu.org/api/rest/v1/sections/c0b869f8403c3c1ddb1a4ffd0a25e5ed7349a7aa_id/studentSectionAssociations/students',
         headers={
             'Accept': 'application/vnd.slc+json',
             'Content-Type': 'application/vnd.slc+json',
